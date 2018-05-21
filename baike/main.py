97cc5db3fee1d87b108250dbfee25bb17984c4e9
likui <826363000@qq.com>
2018-05-21 23:39:15 +0800

    使用多进程进行效率优化

--- baike/main.py
+++ baike/main.py
@@ -1,12 +1,11 @@
-from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
+from multiprocessing import Pool, cpu_count
 
 from baike import url_manager, html_parser, html_downloader, html_outputer
 
 
 class SpiderMain(object):
     def __init__(self):
-        self.thread_pool = ThreadPoolExecutor()
         self.urls = url_manager.UrlManager()
         self.downloader = html_downloader.HtmlDownLoader()
         self.praser = html_parser.HtmlPraser()
@@ -19,38 +18,40 @@ class SpiderMain(object):
         self.urls.add_new_url(root_url)
         # 当前的条数
         count = 1
+        self.task(count)
+        count = count + 1
 
         time1 = datetime.now()
         # 遍历所有的url
-
+        pool = Pool(cpu_count())
         while self.urls.has_new_url():
-            try:
-                # 获取一条url
-                new_url = self.urls.get_new_url()
-
-                print("%d : %s" % (count, new_url))
-
-                # 下载网页
-                html_cont = self.downloader.download(new_url)
-
-                # 解析网页，得到新的url列表和数据
-                new_urls, new_data = self.praser.prase(new_url, html_cont)
-                # 将url列表添加的url管理器
-                self.urls.add_new_urls(new_urls)
-                # 收集数据
-                self.outputer.collect_data(new_data)
-
-                if count == 99:
-                    break
-
-                count = count + 1
-            except(Exception) as e:
-                print("craw fail:%s" % (e))
+            pool.apply_async(self.task, args=(count,))
+            if count == 999:
+                break
+            count = count + 1
+        pool.close()
+        pool.join()
 
         # 输出收集好的数据
         # self.outputer.output_html()
         time2 = datetime.now()
         print("耗时 %s 秒" % (time2 - time1).seconds)  # 23-21
+
+    def task(self, count):
+        try:
+            # 获取一条url
+            new_url = self.urls.get_new_url()
+            print("%d : %s" % (count, new_url))
+            # 下载网页
+            html_cont = self.downloader.download(new_url)
+            # 解析网页，得到新的url列表和数据
+            new_urls, new_data = self.praser.prase(new_url, html_cont)
+            # 将url列表添加的url管理器
+            self.urls.add_new_urls(new_urls)
+            # 收集数据
+            self.outputer.collect_data(new_data)
+        except(Exception) as e:
+            print("craw fail:%s" % (e))
 
 
 if __name__ == "__main__":
---
+++ meizi/main.py
@@ -0,0 +1,51 @@
+import datetime
+import multiprocessing
+import time
+import urllib
+from multiprocessing import Pool, cpu_count
+from urllib import request
+
+import chardet
+from bs4 import BeautifulSoup
+
+
+def downloadImage(imageUrl):
+    print("开始下载%s" % imageUrl)
+    req = urllib.request.Request(imageUrl)
+    req.add_header("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)")
+    req.add_header("Referer", "http://i.meizitu.net")
+    f = open("%s.jpg" % str(time.time()), "wb")
+    f.write(request.urlopen(req).read())
+    f.close()
+
+
+def getHtml(url):
+    req = urllib.request.Request(url)
+    req.add_header("User-Agent",
+                   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
+    req.add_header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+    req.add_header("Accept-Encoding", "gzip, deflate, sdch")
+    req.add_header("Accept-Language", "zh-CN,zh;q=0.8")
+    data = urllib.request.urlopen(req).read()
+    print(chardet.detect(data))
+    return data.decode('GB2312', 'igonre')
+
+
+if __name__ == "__main__":
+
+    url = "http://www.meizitu.com/a/pure_1.html"
+    result = getHtml(url)
+
+    soup = BeautifulSoup(result, 'html.parser')
+    all_a = soup.find('ul', class_='wp-list clearfix').find_all(
+        "img")  # .find_all('a', target='_blank')#,href=re.compile('limg.jpg')
+    pool = Pool(multiprocessing.cpu_count())
+    t1 = datetime.datetime.now()
+    for a in all_a:
+        src = a['src']
+        pool.apply_async(downloadImage, args=(src,))
+    pool.close()
+    pool.join()
+
+    t2 = datetime.datetime.now()
+    print("耗时 %s 秒" % (t2 - t1).seconds)  # 23-21
